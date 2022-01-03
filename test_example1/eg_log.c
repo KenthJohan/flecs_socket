03f441e552954588be9552ab8b731a13915b893a
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2022-01-03 20:40:11 +0100

    test

--- test_example1/eg_log.c
+++ test_example1/eg_log.c
@@ -29,19 +29,57 @@ static char const * get_text(int level)
 	}
 }
 
-enum
-{
-	Q_FREE,
-	Q_STDIN,
-	Q_STDERR,
-	Q_COUNT
-};
-#define QN 64
-#define CAP 1024
-ck_ring_t ring[Q_COUNT];
-ck_ring_buffer_t * buffer[Q_COUNT];
-char * g_msg;
 
+enum channel
+{
+	CHANNEL_FREE,
+	CHANNEL_STDOUT,
+	CHANNEL_STDERR,
+	CHANNEL_COUNT
+};
+#define QUEUE_CAPACITY 64
+#define LOGMSG_CAPACITY 1024
+static ck_ring_t g_ring[CHANNEL_COUNT];
+static ck_ring_buffer_t * g_rbuffer[CHANNEL_COUNT];
+static char * g_msg;
+
+
+static void enqueue_blocking(enum channel q, intptr_t index)
+{
+	ck_backoff_t backoff = CK_BACKOFF_INITIALIZER;
+	bool rv;
+enqueue:
+	rv = ck_ring_enqueue_mpmc(g_ring+q, g_rbuffer[q], (void*)index);
+	if (CK_CC_UNLIKELY(q == CHANNEL_FREE && rv == false))
+	{
+		fprintf(stderr, "Failure to enqueue the free channel should never happen.");
+	}
+	if (CK_CC_UNLIKELY(rv == false))
+	{
+		ck_pr_stall();
+		ecs_os_sleep(0, backoff);
+		ck_backoff_eb(&backoff);
+		goto enqueue;
+	}
+}
+
+
+static intptr_t dequeue_blocking(enum channel q)
+{
+	intptr_t index;
+	ck_backoff_t backoff = CK_BACKOFF_INITIALIZER;
+	bool rv;
+enqueue:
+	rv = ck_ring_dequeue_mpmc(g_ring+q, g_rbuffer[q], (void*)&index);
+	if(rv == false)
+	{
+		ck_pr_stall();
+		ecs_os_sleep(0, backoff);
+		ck_backoff_eb(&backoff);
+		goto enqueue;
+	}
+	return index;
+}
 
 
 
@@ -52,17 +90,14 @@ static void eg_log_msg1(int32_t level, const char *file, int32_t line, const cha
 {
 	char const * color = get_color(level);
 	char const * text = get_text(level);
+	char * buf;
+	intptr_t index = INTPTR_MAX;
 
-	bool rv;
-	intptr_t index;
-	printf("ringsize1: %i\n", ck_ring_size(ring+Q_FREE));
-	do
 	{
-		rv = ck_ring_dequeue_mpmc(ring+Q_FREE, buffer[Q_FREE], (void*)&index);
+		index = dequeue_blocking(CHANNEL_FREE);
+		buf = g_msg + LOGMSG_CAPACITY * index;
 	}
-	while(rv == false);
-	printf("ringsize2: %i\n", ck_ring_size(ring+Q_FREE));
-	char * buf = g_msg + CAP * index;
+
 	char indent[32] = {'\0'};
 	if (level >= 0)
 	{
@@ -78,65 +113,60 @@ static void eg_log_msg1(int32_t level, const char *file, int32_t line, const cha
 		}
 	}
 
-	int q;
+	enum channel q;
 	if (level < 0)
 	{
-		snprintf(buf, 1024, "%s%s"ECS_NORMAL": %s%s:%d: %s", color, text, indent, file, line, msg);
-		q = Q_STDERR;
+		snprintf(buf, LOGMSG_CAPACITY, "%s%s"ECS_NORMAL": %s%s:%d: %s", color, text, indent, file, line, msg);
+		q = CHANNEL_STDERR;
 	}
 	else
 	{
-		snprintf(buf, 1024, "%s%s"ECS_NORMAL":%s %s", color, text, indent, msg);
-		q = Q_STDIN;
+		snprintf(buf, LOGMSG_CAPACITY, "%s%s"ECS_NORMAL":%s %s", color, text, indent, msg);
+		q = CHANNEL_STDOUT;
 	}
 
-	rv = ck_ring_enqueue_mpmc(ring+q, buffer[q], (void*)index);
-	printf("rv: %i %i\n", rv, index);
+	enqueue_blocking(q, index);
+}
+
+
+
+
+
+
+
+
+static bool consume_print(enum channel q)
+{
+	intptr_t index;
+	bool rv;
+	rv = ck_ring_dequeue_mpmc(g_ring+q, g_rbuffer[q], (void*)&index);
+	if (rv == true)
+	{
+		char * buf = g_msg + LOGMSG_CAPACITY * index;
+		printf("Q%i: %s\n", q, buf);
+		enqueue_blocking(CHANNEL_FREE, index);
+	}
+	return rv;
 }
 
 
 static void * thread1(void *arg)
 {
-	bool rv1;
-	bool rv2;
+	ck_backoff_t backoff = CK_BACKOFF_INITIALIZER;
 	while(1)
 	{
-		ecs_os_sleep(1,0);
-		intptr_t index1;
-		intptr_t index2;
-		rv1 = ck_ring_dequeue_mpmc(ring+Q_STDIN, buffer[Q_STDIN], (void*)&index1);
-		rv2 = ck_ring_dequeue_mpmc(ring+Q_STDERR, buffer[Q_STDERR], (void*)&index2);
-
-		if(rv1)
+		//ecs_os_sleep(1,0);
+		bool rv = consume_print(CHANNEL_STDERR) || consume_print(CHANNEL_STDOUT);
+		if (rv == true)
 		{
-			char * buf = g_msg + CAP * index1;
-			printf("thread1: %s\n", buf);
+			backoff = CK_BACKOFF_INITIALIZER;
 		}
-		if(rv2)
+		else
 		{
-			char * buf = g_msg + CAP * index2;
-			printf("thread1: %s\n", buf);
+			ck_pr_stall();
+			ecs_os_sleep(0, backoff);
+			ck_backoff_eb(&backoff);
 		}
-
-		if (rv1)
-		{
-			do
-			{
-				rv1 = ck_ring_enqueue_mpmc(ring+Q_FREE, buffer[Q_FREE], (void*)index1);
-			}
-			while(rv1 == false);
-		}
-
-		if (rv2)
-		{
-			do
-			{
-				rv2 = ck_ring_enqueue_mpmc(ring+Q_FREE, buffer[Q_FREE], (void*)index2);
-			}
-			while(rv2 == false);
-		}
-
-
 	}
 }
 
@@ -144,27 +174,20 @@ void FlecsComponentsEgLogImport(ecs_world_t *world)
 {
 	ECS_MODULE(world, FlecsComponentsEgLog);
 	ecs_set_name_prefix(world, "Eg");
-
-
-
-	ck_ring_init(ring + Q_FREE, QN);
-	ck_ring_init(ring + Q_STDIN, QN);
-	ck_ring_init(ring + Q_STDERR, QN);
-	buffer[Q_FREE] = ecs_os_calloc(sizeof(ck_ring_buffer_t) * QN);
-	buffer[Q_STDIN] = ecs_os_calloc(sizeof(ck_ring_buffer_t) * QN);
-	buffer[Q_STDERR] = ecs_os_calloc(sizeof(ck_ring_buffer_t) * QN);
-	g_msg = ecs_os_calloc(sizeof(char) * CAP * QN);
-
-	for(intptr_t i = 0; i < QN; ++i)
+	for (int i = 0; i < CHANNEL_COUNT; ++i)
 	{
-		ck_ring_enqueue_mpmc(ring + Q_FREE, buffer[Q_FREE], (void*)i);
+		ck_ring_init(g_ring + i, QUEUE_CAPACITY);
+		g_rbuffer[i] = ecs_os_calloc(sizeof(ck_ring_buffer_t) * QUEUE_CAPACITY);
 	}
+	g_msg = ecs_os_calloc(sizeof(char) * LOGMSG_CAPACITY * QUEUE_CAPACITY);
 
-
+	for(intptr_t i = 0; i < QUEUE_CAPACITY; ++i)
+	{
+		int q = CHANNEL_FREE;
+		ck_ring_enqueue_mpmc(g_ring + q, g_rbuffer[q], (void*)i);
+	}
 	ecs_os_api.log_ = eg_log_msg1;
 	ecs_os_thread_t t = ecs_os_thread_new(thread1, NULL);
-
-
 }
 
 
